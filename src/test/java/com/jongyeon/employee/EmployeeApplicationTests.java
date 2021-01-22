317db205edab84aa54f3af69e53816fce4663fce
jongyeon95 <31908589+jongyeon95@users.noreply.github.com>
2021-01-22 17:20:52 +0900

    Initial commit

---
+++ src/test/java/com/jongyeon/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongyeon.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
